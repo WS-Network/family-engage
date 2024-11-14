a6c60b52bcace3ceb9d4295f78b1d53391fc621c
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-14 15:52:54 +0200

    Improved the login/signup design and centered it

--- app/(public)/account/layout.tsx
+++ app/(public)/account/layout.tsx
@@ -1,22 +1,23 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { auth } from '_helpers/server';
-import { Alert } from '_components';
+import { auth } from "_helpers/server";
+import { Alert } from "_components";
 
 export default Layout;
 
 function Layout({ children }: { children: React.ReactNode }) {
-    // if logged in redirect to home page
-    if (auth.isAuthenticated()) {
-        redirect('/');
-    }
+  // if logged in redirect to home page
+  if (auth.isAuthenticated()) {
+    redirect("/");
+  }
 
-    return (
-        <>
-            <Alert />
-            <div className="col-md-6 offset-md-3 mt-5">
-                {children}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Alert />
+      <div className="w-100 min-vh-100 bg-light d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-dark fw-bolder mb-3">Family Engage</h1>
+        {children}
+      </div>
+    </>
+  );
 }
